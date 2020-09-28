193a034136b8857ec395ee722e98e00fa3c7b62f
Svetlin Bilyanov <sbilyanov@gmail.com>
2020-09-28 15:08:32 +0100

    2 Data Types and Variables-Lab

---
+++ 2 Data Types and Variables-Lab/1 Convert Meters to Kilometers/ConcatNames/ConcatNames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConcatNames
+{
+    class ConcatNames
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            string thrid = Console.ReadLine();
+
+            Console.WriteLine($"{first}{thrid}{second}");
+        }
+    }
+}
---
+++ 2 Data Types and Variables-Lab/1 Convert Meters to Kilometers/ReversedChars/ReversedChars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReversedChars
+{
+    class ReversedChars
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            string thrid = Console.ReadLine();
+
+            Console.WriteLine($"{thrid} {second} {first}");
+        }
+    }
+}
