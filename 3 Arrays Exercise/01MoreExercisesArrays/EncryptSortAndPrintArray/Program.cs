1bbe09c7755b2db2d1f7adf69b01e9a2f351fadb
Svetlin Bilyanov <sbilyanov@gmail.com>
2020-10-01 15:11:55 +0100

    3 Arrays Exercise

---
+++ 3 Arrays Exercise/01MoreExercisesArrays/EncryptSortAndPrintArray/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace EncryptSortAndPrintArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            string vowels = "EeUuIiOoAa";
+            int[] arraySum = new int[lines];
+
+
+
+
+            for (int i = 0; i < lines; i++)
+            {
+                char[] currentName = Console.ReadLine().ToCharArray();
+                int sumVowels = 0;
+                int sumConsonant = 0;
+
+
+                int sum =0;
+                for (int j = 0; j < currentName.Length; j++)
+                {
+                    char currentletter = currentName[j];
+
+                    if (vowels.Contains(currentletter))
+                    {
+                        sumVowels += currentletter*currentName.Length;
+
+                    }
+                    else
+                    {
+                        sumConsonant += currentletter/currentName.Length;
+                    }
+
+                    sum = sumVowels + sumConsonant;
+                      arraySum[i] = sum;
+                }
+            }
+            Array.Sort(arraySum);
+            for (int i = 0; i < arraySum.Length; i++)
+            {
+                Console.WriteLine(arraySum[i]);
+            }
+        }
+       
+    }
+}
---
+++ 3 Arrays Exercise/ArrayRotation/ArrayRotation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArrayRotation
+{
+    class ArrayRotation
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine()
+                .Split();
+            int n = int.Parse(Console.ReadLine());
+         
+
+            for (int i = 0; i <n; i++)
+            {
+                string routatingNum = array[0];
+                for (int j = 1; j < array.Length; j++)
+                {
+                    string currentElement = array[j];
+                    array[j - 1] = currentElement;
+                }
+
+                array[array.Length - 1] = routatingNum;
+            }
+
+            Console.WriteLine(string.Join(' ', array)); 
+
+        
+        }
+    }
+}
