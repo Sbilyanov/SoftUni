6f9f23fbd0ab8fb20c45365f8d0ab7dafd54f9e3
Svetlin Bilyanov <sbilyanov@gmail.com>
2020-10-02 15:12:47 +0100

    4. Methods

---
+++ 4. Methods/RepeatString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int times = int.Parse(Console.ReadLine());
+
+            Printtext(text, times);
+        }
+
+        private static void Printtext(string str, int num)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < num; i++)
+            {
+                result.Append(str);
+            }
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
