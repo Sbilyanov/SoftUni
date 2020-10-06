4233ffaa6678b785569eda45d856ac8e48af1727
Svetlin Bilyanov <sbilyanov@gmail.com>
2020-10-06 15:13:34 +0100

    4. Methods Exercise

---
+++ 4. Methods Exercise/CharactersInRange/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first =char.Parse(Console.ReadLine());
+            char second =Convert.ToChar(Console.ReadLine());
+
+            PrintCharsFromStartToEnd(first, second);
+
+           
+        }
+
+        private static void PrintCharsFromStartToEnd(char first, char second)
+        {
+
+            char start;
+            char end;
+
+            if (first < second) 
+            {
+                start = first;
+                end = second;
+            }
+            else
+            {
+                start = second;
+                end = first;
+            }
+            for (int i = start + 1; i < end; i++)
+            {
+
+                Console.Write($"{(char)(i)} ");
+            }
+        }
+    }
+}
---
+++ 4. Methods Exercise/PasswordValidator/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace PasswordValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+
+            bool passwordIsValid = ValidatePasswordLength(password) &&
+                                   ValidatePasswordIsLetterOrDigits(password) &&
+                                   ValidatePasswordIsTwoDigits(password);
+            if (passwordIsValid)
+            {
+                Console.WriteLine("Password is valid");
+            }
+            if (!passwordIsValid)
+            {
+                if (!ValidatePasswordLength(password))
+                {
+                    Console.WriteLine("Password must be between 6 and 10 characters");
+                }
+                if (!ValidatePasswordIsLetterOrDigits(password))
+                {
+                    Console.WriteLine("Password must consist only of letters and digits");
+                }
+                if (!ValidatePasswordIsTwoDigits(password))
+                {
+                    Console.WriteLine("Password must have at least 2 digits");
+                }
+
+            }
+        }
+
+        private static bool ValidatePasswordIsTwoDigits(string password)
+        {
+            int counter = 0;
+            foreach (char c in password)
+            {
+                if (char.IsDigit(c))
+                {
+                    counter++;
+                }
+            }
+            if (counter >= 2)
+            {
+                return true;
+            }          
+                return false;
+        }
+
+        private static bool ValidatePasswordIsLetterOrDigits(string password)
+        {
+            foreach (char c in password)
+            {
+                if (!char.IsLetterOrDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool ValidatePasswordLength(string password)
+        {
+            if (password.Length >= 6 && password.Length <= 10)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
