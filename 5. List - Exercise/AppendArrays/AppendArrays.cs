9a114345a4f8b425b682b6dd316db16fd67a6349
Svetlin Bilyanov <sbilyanov@gmail.com>
2020-10-13 15:15:50 +0100

    5. List-Exercise

---
+++ 5. List - Exercise/AppendArrays/AppendArrays.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppendArrays
+{
+    class AppendArrays
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine()
+                .Split('|')
+                .ToList();
+
+            numbers.Reverse();
+
+            List<string> result = new List<string>();
+
+            foreach (string items in numbers)
+            {
+                string[] array = items.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string index in array)
+                {
+                    result.Add(index);
+
+                }
+
+
+            }
+            Console.WriteLine(string.Join(' ',result));
+        }
+    }
+}
---
+++ 5. List - Exercise/Train/Train.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Train
+{
+    class Train
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            string[] command = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            while (command[0].ToUpper() != "END")
+            {
+                if (command[0].ToUpper() == "ADD")
+                {
+                    wagons.Add(int.Parse(command[1]));
+                }
+                else
+                {
+                    int passenger = int.Parse(command[0]);
+                    for (int i = 0; i < wagons.Count; i++)                     
+                   {
+                        int n = wagons[i];
+
+                        if ( passenger+n <= maxCapacity)
+                        {
+                           wagons[i]+= passenger;
+                            break;
+
+                        }
+                    }
+                  
+                  
+                }
+
+             command = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            }
+            Console.WriteLine(string.Join(' ', wagons));
+
+        }
+    }
+}
