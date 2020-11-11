524fd5541e8a3b730f9ad48916bf3449efaf7c49
Svetlin Bilyanov <sbilyanov@gmail.com>
2020-11-11 15:19:48 +0000

    Mid Exam - 5 July 2020

---
+++ Mid Exam - 5 July 2020/Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                 .Select(int.Parse)
+                 .ToList();
+
+            if (numbers.Count<5)
+            {
+                Console.WriteLine("No");
+                return;
+
+            }
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("No");
+                return;
+            }
+            double average = numbers.Average();
+
+            var result = numbers.OrderByDescending(x => x).Where(x => x > average).Take(5)
+
+            Console.WriteLine(string.Join(' ',result));
+           
+        }
+    }
+}
