c693b260de1077c50f82b8b1a036a09d5f5809bd
Svetlin Bilyanov <sbilyanov@gmail.com>
2020-11-18 15:47:16 +0000

    8. Strings and Text Processing- Lab

---
+++ C# Fundamentals/8. Strings and Text Processing/RepeatString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                 .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string output = string.Empty;
+
+            foreach (var word in input)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    output += word;
+
+                }
+
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ C# Fundamentals/8. Strings and Text Processing/ReverseStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+
+                string output = new string(input.Reverse().ToArray());
+                string result=$"{input} = {output}";
+                
+                Console.WriteLine($"{input} = {output}");
+               
+
+                input = Console.ReadLine();
+            }
+
+          
+        }
+    }
+}
