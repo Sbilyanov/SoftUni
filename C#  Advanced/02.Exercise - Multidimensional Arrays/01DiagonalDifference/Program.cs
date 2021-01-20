d09b4c5b187f68021e44f0660c6cd44a53dd836c
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-20 23:17:53 +0000

    02.Exercise - Multidimensional Arrays

---
+++ C#  Advanced/02.Exercise - Multidimensional Arrays/01DiagonalDifference/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _01DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] marix = new int[n,n];
+
+            for (int row = 0; row < marix.GetLength(0); row++)
+            {
+                int[] dataMatrix = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < marix.GetLength(1); col++)
+                {
+                    marix[row,col]=dataMatrix[col];
+                }
+            }
+
+            int firstDiagonalSum = 0;
+            int SecondtDiagonalSum = 0;
+          
+
+            for (int i = 0; i < n; i++)
+            {
+                firstDiagonalSum += marix[i, i];           
+                SecondtDiagonalSum += marix[i,n-i-1];
+            }
+            Console.WriteLine(Math.Abs(firstDiagonalSum-SecondtDiagonalSum));
+        }
+    }
+}
