d62f53a9c382eda2c2ba55490d62dcad9cfb0288
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-25 19:36:21 +0000

    added new task

---
+++ C#  Advanced/04. Streams, Files and Directories - Lab/02.LineNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                string currentLine = reader.ReadLine();
+                int row = 1;
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (currentLine!=null)
+                    {
+                        writer.WriteLine($"{row}. {currentLine}");
+
+                        row++;
+
+                        currentLine = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
