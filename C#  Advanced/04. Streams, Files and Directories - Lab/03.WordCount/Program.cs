be1549ec42205039d787e7f313135cd737fda323
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-25 19:37:23 +0000

    added task 03

---
+++ C#  Advanced/04. Streams, Files and Directories - Lab/03.WordCount/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _03.WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            using (StreamReader reader = new StreamReader("../../../input.txt"))
+            {
+                using (StreamReader readerWords = new StreamReader("../../../words.txt"))
+                {
+                    string[] words = readerWords.ReadLine().Split();
+
+                    string[] text = reader.ReadToEnd().Split(new[] { ' ', ',', '.', '!', '?', '-' });
+
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        int counter = 1;
+                        for (int j = 0; j < text.Length; j++)
+                        {
+
+                            if (words[i] == text[j])
+                            {
+                              
+                                if (!result.ContainsKey(words[i]))
+                                {
+                                    result.Add(words[i], counter);
+                                }
+                                result[words[i]]++;
+                            }
+                        }
+                        using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                        {
+                            writer.WriteLine($"{words[i]} - {counter}");
+                        }
+
+                    }
+
+                }
+
+
+            }
+        }
+    }
+}
---
+++ C#  Advanced/04. Streams, Files and Directories - Lab/03.WordCount/System/IO/Directory.cs
@@ -0,0 +1,9 @@
+﻿namespace System.IO
+{
+    internal class Directory<T1, T2>
+    {
+        public Directory()
+        {
+        }
+    }
+}
