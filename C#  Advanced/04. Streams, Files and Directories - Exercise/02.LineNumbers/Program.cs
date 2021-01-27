9613953dd0118a126ff7023dee49594c3868591a
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-27 20:27:40 +0000

    task02.

---
+++ C#  Advanced/04. Streams, Files and Directories - Exercise/02.LineNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            string outputPath = Path.Combine("..", "..", "..", "output.txt");
+
+            int lineCounter = 1;
+
+            foreach (string words in lines)
+            {
+
+                int punctuationCounter = 0;
+                int letterCounter = 0;
+
+               
+                foreach (char ch in words)
+                {
+                    if (char.IsLetter(ch))
+                    {
+                        letterCounter++;
+                    }
+                    else if (char.IsPunctuation(ch))
+                    {
+                        punctuationCounter++;
+                    }
+
+                }
+
+                string output = $"Line {lineCounter}: {words}({letterCounter})({punctuationCounter})";
+       
+                File.AppendAllText(outputPath, output + Environment.NewLine);
+            }
+        }
+    }
+}
