05507a39bd0c4939ab61cef12f8e5cd571749b40
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-27 20:26:29 +0000

    04. Streams, Files and Directories - Exercise

---
+++ C#  Advanced/04. Streams, Files and Directories - Exercise/01.EvenLines/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01.EvenLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader =new StreamReader("input.txt"))
+            {
+               using StreamWriter writer = new StreamWriter("output.txt");
+
+                string line = reader.ReadLine();
+                string pattern = @"[-,.!?]";
+
+                int counter = 0;
+
+                while (line != null)
+                {
+                    if (counter++ % 2 == 0)
+                    {
+                        string replaced = Regex.Replace(line, pattern, "@");
+
+                        string[] output = replaced.Split();
+
+                        writer.WriteLine(string.Join(' ', output.Reverse()));
+                    }
+
+                    line = reader.ReadLine();
+                }
+
+            }
+        }
+    }
+}
