d1423bed4ff87a5265537a855c57bc7bc1c387be
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-28 15:44:06 +0100

    07.TwoZipAndExtract

---
+++ C#  Advanced/04. Streams, Files and Directories - Exercise/07.TwoZipAndExtract/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _07.TwoZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path =Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "copyMeTwo.zip");
+            using (ZipArchive archive = ZipFile.Open(path, ZipArchiveMode.Create)) 
+            {
+                archive.CreateEntryFromFile("copyMeTwo.png", "copY.png");
+            }
+        }
+    }
+}
