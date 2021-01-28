8445f423c1d25236206b8470e8e57d5dc3f750a3
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-28 15:43:25 +0100

    06.ZipAndExtract

---
+++ C#  Advanced/04. Streams, Files and Directories - Exercise/06.ZipAndExtract/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+            string filePath = Path.Combine("..","..","..","output.zip");
+            string file = "copyMe.png";
+
+            using ZipArchive zipArchive = ZipFile.Open(filePath, ZipArchiveMode.Create);
+                 zipArchive.CreateEntryFromFile(file, Path.GetFileName(file));
+                
+        }
+    }
+}
