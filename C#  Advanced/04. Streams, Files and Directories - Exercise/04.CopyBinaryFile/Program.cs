969026356e2752099cef6032e1fb67a73ac9cd04
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-28 15:41:54 +0100

    04.CopyBinaryFile

---
+++ C#  Advanced/04. Streams, Files and Directories - Exercise/04.CopyBinaryFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _04.CopyBinaryFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream reader = new FileStream("copyMe.png", FileMode.Open);
+            using FileStream writer = new FileStream("../../../newImage.png", FileMode.Create);
+
+            while (true)
+            {
+                byte[] buffer = new byte[4096];
+                int count = reader.Read(buffer, 0, buffer.Length);
+
+                if (count==0)
+                {
+                    break;
+                }
+                writer.Write(buffer);
+            }
+        }
+    }
+}
