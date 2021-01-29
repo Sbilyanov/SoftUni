091e2adad7b5c5db10431c25c2a6af96625c054b
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-29 23:41:45 +0100

    04.AddVAT

---
+++ C#  Advanced/05. Functional Programming - Lab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> filtter = text => Char.IsUpper(text[0]);
+
+            string[] text = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Where(filtter).ToArray();
+            Console.WriteLine(string.Join(Environment.NewLine,text));
+        }
+    }
+}
---
+++ C#  Advanced/05. Functional Programming - Lab/04.AddVAT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] array = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .Select(n=>n+(n*0.2m)).ToArray();
+            foreach (var price in array)
+            {
+                Console.WriteLine($"{price:f2}");
+            }
+        }
+    }
+}
