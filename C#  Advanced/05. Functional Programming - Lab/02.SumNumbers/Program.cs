f47a399b41ec2c9312eba09beb8007ef794e939a
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-29 23:38:54 +0100

    exercise 2

---
+++ C#  Advanced/05. Functional Programming - Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSumAndCount(int.Parse, a => a.Length, a => a.Sum());
+
+        }
+        static void PrintSumAndCount(Func<string, int> parser
+                                   , Func<int[], int> countGetter
+                                   , Func<int[], int> sumCalculator)
+        {
+
+            int[] array = Console.ReadLine().Split(", ").Select(parser).ToArray();
+
+            Console.WriteLine(countGetter(array));
+            Console.WriteLine(sumCalculator(array));
+        }
+
+
+    }
+}
+
