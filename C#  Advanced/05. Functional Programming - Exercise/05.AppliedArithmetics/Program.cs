5f1bd7b2449d71e4470dea5f6e62f33449eec750
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-30 13:25:10 +0100

    task05 two ways

---
+++ C#  Advanced/05. Functional Programming - Exercise/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            string command = Console.ReadLine();
+            Action<List<int>> print = n => Console.WriteLine(string.Join(' ', n));
+            while (command != "end")
+            {
+                if (command == "print")
+                {
+                    print(numbers);
+                }
+                else
+                {
+                    //numbers.Select(n=>arithmeticFunc(n)).ToList();
+                    Func<int, int> arithmeticFunc = GetAritmeticFunc(command);
+                    numbers = numbers.Select(arithmeticFunc).ToList();
+                }
+
+                command = Console.ReadLine();
+            }
+
+
+        }
+
+        private static Func<int, int> GetAritmeticFunc(string command)
+        {
+            switch (command)
+            {
+                case "add": return x => x + 1;
+                case "subtract": return x => x - 1;
+                case "multiply": return x => x * 2;
+                default: return null;
+
+            }
+        }
+    }
+}
---
+++ C#  Advanced/05. Functional Programming - Exercise/05D.AppliedArithmeticsWithDictionary/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05D.AppliedArithmeticsWithDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            string command = Console.ReadLine();
+
+            Action<List<int>> print = n => Console.WriteLine(string.Join(' ', n));
+
+            Dictionary<string, Func<int, int>> arithmeticDictionary =new Dictionary<string, Func<int, int>>()
+            {
+                { "add", x => x + 1 },
+                { "subtract", x => x - 1 },
+                { "multiply", x => x * 2 }
+             };
+
+            while (command != "end")
+            {
+                if (arithmeticDictionary.ContainsKey(command))
+                {
+                    numbers = numbers.Select(arithmeticDictionary[command]).ToList();
+                }
+
+
+                else if (command == "print")
+                {
+                    print(numbers);
+                }
+              
+
+                command = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
+
