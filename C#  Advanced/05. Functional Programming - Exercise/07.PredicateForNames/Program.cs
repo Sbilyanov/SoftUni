940c3c3d3b6ceca2177dabd18aa3627c69d6a50a
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-30 13:26:27 +0100

    task07

---
+++ C#  Advanced/05. Functional Programming - Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).ToList();
+            // Predicate<string> predicate = name => name.Length <= n;
+            // List<string> sortedNames = names.Where(n=>predicate(n)).ToList();  
+
+            Func<string, int, bool> func = (name, length) => name.Length <= length;
+            List<string> sortedNames = names.Where(names=>func(names,n)).ToList();  
+            
+            Action<List<string>> print = n => Console.WriteLine(string.Join(Environment.NewLine,n));
+            print(sortedNames);
+        }
+    }
+}
