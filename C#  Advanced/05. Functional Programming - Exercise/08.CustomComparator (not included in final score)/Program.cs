9b967288fe01c8d58ab8489c500e95bca99ffc3c
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-30 13:27:21 +0100

    task08

---
+++ C#  Advanced/05. Functional Programming - Exercise/08.CustomComparator (not included in final score)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.CustomComparator__not_included_in_final_score_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Array.Sort(numbers, (a, b) =>
+              {
+                  if (a % 2 != 0 && b % 2 == 0)
+                  {
+                      return 1;
+                  }
+                  else if (a % 2 == 0 && b % 2 != 0)
+                  {
+                      return -1;
+                  }
+                  int temp = a.CompareTo(b);
+                  return temp;
+
+              });
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
