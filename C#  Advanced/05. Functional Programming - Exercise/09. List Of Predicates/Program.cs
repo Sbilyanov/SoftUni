e8f597a40265dec380abb81398070ff57f175fd0
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-30 13:28:32 +0100

    task09

---
+++ C#  Advanced/05. Functional Programming - Exercise/09. List Of Predicates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.ListOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int end = int.Parse(Console.ReadLine());
+            List<int> dividers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            Func<int, int, bool> predicate = (num, div) => num % div == 0;
+
+            for (int i = 1; i <= end; i++)
+            {
+                if (dividers.All(div=>predicate(i,div)))
+                {
+                    Console.Write(i+" ");
+                }
+            }
+        }
+    }
+}
