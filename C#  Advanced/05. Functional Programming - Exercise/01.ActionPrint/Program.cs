8aa9a5679ffe441dc6334343458145e5acd1c7ac
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-30 13:21:13 +0100

    task01

---
+++ C#  Advanced/05. Functional Programming - Exercise/01.ActionPrint/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01.ActionPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string[]> printing = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            printing(input);
+        }
+    }
+}
