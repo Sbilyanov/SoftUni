ee79df9a3ead2ebe73b5d8c99b2ee52956514114
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-01-30 13:22:29 +0100

    task02

---
+++ C#  Advanced/05. Functional Programming - Exercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(name=>$"Sir {name}").ToList();
+
+            Action<List<string>> printing = n => Console.WriteLine(string.Join(Environment.NewLine,n));
+
+            printing(input);
+        }
+    }
+}
