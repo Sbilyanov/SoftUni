caf7ed5cb0ecb516e41494df264f242db393c8db
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-02-01 20:04:15 +0000

    05

---
+++ C#  Advanced/06. Defining Classes - Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.DateModifier
+{
+    public static class DateModifier
+    {
+      public static int  GetDateModifier(string fristDateStr,string secondDateStr)
+        {
+            DateTime fristDate = DateTime.Parse(fristDateStr);
+            DateTime secondDate = DateTime.Parse(secondDateStr);
+
+            TimeSpan differernce = fristDate - secondDate;
+
+            return differernce.Days;
+        }
+    }
+}
---
+++ C#  Advanced/06. Defining Classes - Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05.DateModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstDate = Console.ReadLine();
+            string secondDate = Console.ReadLine();
+
+            int result=DateModifier.GetDateModifier(firstDate, secondDate);
+
+            Console.WriteLine(Math.Abs(result));
+
+        }
+    }
+}
