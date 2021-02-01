9828b7ac5f225fed46cc28254a64f7f5e6960d81
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-02-01 20:03:26 +0000

    04

---
+++ C#  Advanced/06. Defining Classes - Exercise/04.OpinionPoll/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] current = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(current[0], int.Parse(current[1]));
+
+                people.Add(person);
+
+            }
+
+           List<Person> sortedPeople= people.OrderBy(n => n.Name).Where(a => a.Age > 30).ToList();
+
+            foreach (var item in sortedPeople)
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            };
+            
+        }
+    }
+}
