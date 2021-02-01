f99a2b54c3df0c0bdbfe61acc88c7da39370aa36
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-02-01 20:02:50 +0000

    03

---
+++ C#  Advanced/06. Defining Classes - Exercise/03.OldestFamilyMember/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] current = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(current[0], int.Parse(current[1]));
+
+                family.AddMember(person);
+
+            }
+
+            Person oldestPerson = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+
+        }
+    }
+}
