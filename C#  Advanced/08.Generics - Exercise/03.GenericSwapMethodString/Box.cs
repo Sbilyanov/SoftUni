d597bc404bae8167b94d0ea1c35795678f67585e
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-02-08 21:11:16 +0000

    03

---
+++ C#  Advanced/08.Generics - Exercise/03.GenericSwapMethodString/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.GenericSwapMethodString
+{
+    class Box<T>
+    {
+
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+        public T Value { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType().FullName}: {this.Value}";
+        }
+
+        public void IndexSwap(List<Box<T>>boxes,int firstIndex,int secondIndex)
+        {
+            Box<T> temp = boxes[firstIndex];
+            boxes[firstIndex] = boxes[secondIndex];
+            boxes[secondIndex] = temp;
+        }
+    }
+}
