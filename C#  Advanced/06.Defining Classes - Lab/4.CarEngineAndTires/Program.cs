f78cc127f01fdf80870d5159f239547fc045f847
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-02-08 14:25:37 +0100

    04

---
+++ C#  Advanced/06.Defining Classes - Lab/4.CarEngineAndTires/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Tire[] tires = new Tire[4]
+                {
+                    new Tire (2020, 2.3),               
+                    new Tire (2020, 2.2),                
+                    new Tire (2020 , 2.1),          
+                    new Tire (2020 , 2.4)
+                };
+
+            Engine engine = new Engine(500, 5000);
+
+            Car car = new Car("BMW", "M6", 2010, 100, 20, engine, tires);
+
+
+            Console.WriteLine("Horse power: " + car.Engine.HorsePower);
+
+            foreach (var tire in car.Tires)
+            {
+                Console.WriteLine($"{tire.Year} - {tire.Pressure}");
+            }
+        }
+    }
+}
