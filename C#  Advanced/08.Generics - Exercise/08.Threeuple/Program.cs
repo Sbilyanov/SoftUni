301f7d4405c913a9c155f51eaeb3a5f795af8f64
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-02-08 21:13:47 +0000

    08

---
+++ C#  Advanced/08.Generics - Exercise/08.Threeuple/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.Threeuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] threeupleOneData = Console.ReadLine().Split();
+            string fullName = $"{threeupleOneData[0]} {threeupleOneData[1]}";
+            List<string> townData = threeupleOneData.Skip(3).ToList();
+            string town = string.Join(' ', townData);
+            Threeuple<string, string,string> threeupleOne = new Threeuple<string, string, string>
+                (fullName, threeupleOneData[2],town);
+
+            string[] threeupleTwoData = Console.ReadLine().Split();
+            bool drunk = threeupleTwoData[2] == "drunk" ? true : false;
+
+ 
+            Threeuple<string, int, bool> threeupleTwo = new Threeuple<string, int, bool>
+                (threeupleTwoData[0], int.Parse(threeupleTwoData[1]), drunk);
+
+            string[] threeupleThreeData = Console.ReadLine().Split();
+            Threeuple<string, double, string> threeupleThree = new Threeuple<string, double, string>
+                (threeupleThreeData[0], double.Parse(threeupleThreeData[1]), threeupleThreeData[2]);
+
+            Console.WriteLine(threeupleOne);
+            Console.WriteLine(threeupleTwo);
+            Console.WriteLine(threeupleThree);
+        }
+    }
+}
