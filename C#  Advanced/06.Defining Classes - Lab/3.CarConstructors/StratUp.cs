bce422515fcc0a92cee4c6fb46f5fdb719f19776
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-02-08 14:25:04 +0100

    03

---
+++ C#  Advanced/06.Defining Classes - Lab/3.CarConstructors/StratUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year =int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car fristCar = new Car();
+            Car secondCar = new Car(model,make,year);
+            Car thridCar = new Car(model,make,year,fuelQuantity,fuelConsumption);
+
+           
+        }
+    }
+}
