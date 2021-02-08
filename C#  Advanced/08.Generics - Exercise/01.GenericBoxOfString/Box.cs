1b6f247d31d1c213d0a6fb93229a18c73af8d929
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-02-08 21:09:32 +0000

    08.Generics - Exercise

---
+++ C#  Advanced/08.Generics - Exercise/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.GenericBoxOfString
+{
+   public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+        public T Value { get;private set; }
+
+        public override string ToString()
+        {
+            //  Type valueType = Value.GetType();
+            // string valueName = valueType.FullName;
+
+            return $"{Value.GetType().FullName}: {this.Value}";
+        }
+    }
+}
---
+++ C#  Advanced/08.Generics - Exercise/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.GenericBoxOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine());
+                Console.WriteLine(box);
+
+            }
+        }
+    }
+}
