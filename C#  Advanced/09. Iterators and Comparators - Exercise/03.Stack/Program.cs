c3e254aea6051d9e44ffeccb259f7f46b551b0d7
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-02-10 21:17:51 +0000

    03

---
+++ C#  Advanced/09. Iterators and Comparators - Exercise/03.Stack/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<string> stack = new MyStack<string>(Console.ReadLine()
+              .Split(new char[]{' ',',' }, StringSplitOptions.RemoveEmptyEntries).Skip(1).ToList());
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                string[] command = input.Split();
+                switch (command[0])
+                {
+                    case "Pop":
+                        try
+                        {
+                        stack.Pop();
+
+                        }
+                        catch (Exception ex)
+                        {
+
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    case "Push":
+                        for (int i = 1; i < command.Length; i++)
+                        {
+                            stack.Push(command[i]);
+                        } 
+
+                        break;
+
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, stack));
+            Console.WriteLine(string.Join(Environment.NewLine, stack));
+         
+        }
+    }
+}
