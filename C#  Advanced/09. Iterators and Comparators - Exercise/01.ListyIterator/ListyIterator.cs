a6b25b7e3a457a691877a6239214e7f33eecb12b
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-02-10 21:16:31 +0000

    01

---
+++ C#  Advanced/09. Iterators and Comparators - Exercise/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.ListyIterator
+{
+    class ListyIterator<T> 
+    {
+        private List<T> items;
+
+        private int index;
+
+        public ListyIterator(List<T> items)
+        {
+
+            this.items = items;
+
+            this.index = 0;
+
+        }
+
+        public void Print()
+        {
+            if (this.index >= this.items.Count)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+             Console.WriteLine(this.items[this.index]);
+        }
+
+        public bool Move()
+        {
+            bool hasNext = this.HasNext();
+            if (hasNext)
+            {
+               this.index++;
+            }
+            return hasNext;
+        }
+
+        public bool HasNext() => this.index < this.items.Count -1 ;
+        
+    }
+}
---
+++ C#  Advanced/09. Iterators and Comparators - Exercise/01.ListyIterator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> list =new ListyIterator<string>( Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1).ToList());
+
+            string command = Console.ReadLine();
+            while (command!="END")
+            {
+                switch (command)
+                {
+                    case "HasNext":
+                        Console.WriteLine(list.HasNext());
+                        break;
+                    case "Move":
+                        Console.WriteLine(list.Move());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            list.Print();
+                        }
+                        catch (Exception ex )
+                        {
+                            Console.WriteLine(ex.Message);                      
+                        }
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
