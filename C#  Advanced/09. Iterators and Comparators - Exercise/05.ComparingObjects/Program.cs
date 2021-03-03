958459868b314830bd9d3a9a162dd2ab772da4aa
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-03-03 21:18:58 +0000

    05

---
+++ C#  Advanced/09. Iterators and Comparators - Exercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<Person> persons = new List<Person>();
+
+            while (input != "END")
+            {
+                string[] elements = input.Split();
+                Person currentPerson = new Person(elements[0], int.Parse(elements[1]), elements[2]);
+                persons.Add(currentPerson);
+                input = Console.ReadLine();
+            }
+            int n = int.Parse(Console.ReadLine());
+            Person personToCompare = persons[n-1];
+            int equalCount = 0;
+            int notEqualCount = 0;
+            foreach (Person person in persons)
+            {
+                int index = person.CompareTo(personToCompare);
+                if (index != 0)
+                {
+                    notEqualCount++;
+                }
+                else
+                {
+                    equalCount++;
+                }
+
+            }
+            if (equalCount > 1)
+            {
+                Console.WriteLine($"{equalCount} {notEqualCount} {persons.Count}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
