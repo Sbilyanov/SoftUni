cec4d89c4efa303441696c12460886f76e6adb4c
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-03-08 21:12:47 +0000

    06

---
+++ C#  Advanced/08.Generics - Exercise/06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class Box<T>:IComparable
+        where T:IComparable
+    {
+    
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public int CompareTo(object obj)
+        {
+            Box<T> box = obj as Box<T>;
+
+            int compare = this.Value.CompareTo(box.Value);
+
+            return compare;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType().FullName}: {this.Value}";
+        }
+    }
+}
---
+++ C#  Advanced/08.Generics - Exercise/06.GenericCountMethodDouble/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.GenericCountMethodDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Box<double>> boxes = new List<Box<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                Box<double> currentBox = new Box<double>(double.Parse(Console.ReadLine()));
+                boxes.Add(currentBox);
+            }
+
+            Box<double> comparebleBox = new Box<double>(double.Parse(Console.ReadLine()));
+
+            int count = GetCompareCount(boxes,comparebleBox);
+         
+
+            Console.WriteLine(count);
+        }
+
+        private static int GetCompareCount(List<Box<double>> boxes, Box<double> comparebleBox)       
+        {
+            int counter = 0;
+            foreach (var box in boxes)
+            {
+                int compare = box.CompareTo(comparebleBox);
+                if (compare>0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;          
+        }
+    }
+}
