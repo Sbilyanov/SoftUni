8c61b0ec67e7c49de093221f1945aa77381bdfe3
Svetlin Bilyanov <sbilyanov@gmail.com>
2021-03-08 21:10:40 +0000

    02

---
+++ C#  Advanced/08.Generics - Exercise/02.GenericBoxOfInteger/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+        public T Value { get;private set; }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType().FullName}: {this.Value}";
+        }
+    }
+}
